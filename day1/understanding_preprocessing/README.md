1efda1040fe4e50d6e72a050df95a6628195f87d
Praveen Ramanujam <praveen.ramanujam@rapyuta-robotics.com>
2019-11-09 15:09:28 +0900

    understanding preprocessing

---
+++ day1/understanding_preprocessing/README.md
@@ -0,0 +1,23 @@
+
+Commands:
+
+Preprocessing, Compiling and Linking
+
+
+To see the output of preprocessing
+g++ -E preprocessing.cpp
+
+Please change the value of #define to change the preprocessing output
+
+
+
+g++ preprocessing.cpp -o run
+
+
+
+Different arguments that g++ can take:
+
+https://www.geeksforgeeks.org/compiling-with-g-plus-plus/
+
+Difference between gcc and g++?
+
---
+++ day1/understanding_preprocessing/preprocessing.cpp
@@ -0,0 +1,26 @@
+// what does iostream include do?
+#include <iostream>
+
+#define TEST2
+
+// main always returns int and can take int and char* [] as arguments
+int main(int argc, char* argv[])
+{
+
+#ifdef TEST1
+    for(int i = 0; i < argc; i++)
+    {
+        std::cout << argv[i] << std::endl;
+    }
+#endif
+
+#ifdef TEST2
+    for(int i = 0; i < argc; i++)
+    {
+        std::cout << "ARGS": argv[i] << std::endl;
+    }
+#endif
+
+
+    return 0;
+}
