f69442722c16f956e641ecc099ee20b3d2f28e1e
dennyboby123 <denny.boby@rapyuta-robotics.com>
2019-11-30 14:10:30 +0900

    my learning

--- day1/understanding_preprocessing/preprocessing.cpp
+++ day1/understanding_preprocessing/preprocessing.cpp
@@ -1,29 +1,115 @@
 // what does iostream include do?
 #include <iostream>
+#include <chrono>
+
 
 #define TEST
 
-#ifndef TEST
-#define TEST2
-#endif
+// #define PI 3.1415
+// #define DEGTORAD(deg) (deg * PI / 180)
+
+// #undef TEST
+
+// #ifndef TEST
+// #define TEST2
+// #else
+// #define TEST1
+// #endif
+
+template <typename Z, typename T>
+Z sum(Z x, T y) 
+{ 
+   return x + y; 
+}
+
+// int myfunction() 
+// {
+    
+//     static const int i = 5;
+//     static int j;
+    
+//     std::cout <<"normal integer :" << i << std::endl << "static integer :" << j << std::endl;
+//     // i++;
+//     j++;
+    
+    
+    
+//     return i;
+// }
+
+// float sum (const int& a,const int& b)
+// {
+//     return a + b;
+// }
+
+// float sum (const float& a,const int& b)
+// {
+//     return (int)(a + b);
+// }
+
+
 // main always returns int and can take int and char* [] as arguments
 int main(int argc, char* argv[])
 {
+    
+    std::cout << "sum = " << sum(std::string("ab"),std::string("bb")) << std::endl;
+    // double rad = DEGTORAD(90);
+    // std::cout << "radians = " << rad << std::endl;
 
-#ifdef TEST1
-    for(int i = 0; i < argc; i++)
-    {
-        std::cout << argv[i] << std::endl;
-    }
-#endif
+    // int x = 5;
+    // int &i = x;
+    // std::cout << "i = " << *(&i) << " x = " << *(&x) << std::endl;
 
-#ifdef TEST2
-    for(int i = 0; i < argc; i++)
-    {
-        std::cout << "ARGS": argv[i] << std::endl;
-    }
-#endif
+    // int a[5]{5};
+    // int i(5);
 
+    // for(int i=0; i < sizeof(a)/sizeof(int); i++)
+    // {
+    //     std::cout << "index " << i << " has value " << a[i] << std::endl;
+    // }
+
+    // auto start = std::chrono::high_resolution_clock::now();
+    // for (int i=0; i < 10000; i++)  
+    // myfunction();
+
+    // auto finish = std::chrono::high_resolution_clock::now();
+    // std::chrono::duration<double> elapsed = finish - start;
+    // std::cout << "Elapsed time: " << elapsed.count() << std::endl;
+
+    // int a;
+    // bool b;
+    // char c;
+    // float d;
+    // double e;
+    // void f; (You cannot declare a variable of type void.)
+    // wchar_t g;
+    // int16_t h = -32768;
+    // uint16_t i = h;
+    // std::cout << "uint16_t " << i << std::endl;
+
+    // std::cout << "sizeof void " << sizeof(a) << std::endl;
+    // std::cout << "sizeof void " << sizeof(b) << std::endl;
+    // std::cout << "sizeof void " << sizeof(c) << std::endl;
+    // std::cout << "sizeof void " << sizeof(d) << std::endl;
+    // std::cout << "sizeof void " << sizeof(e) << std::endl;
+    // std::cout << "sizeof void" << sizeof(f) << std::endl;
+    // std::cout << "sizeof void" << sizeof(g) << std::endl;
+
+    
+// #if defined(TEST) 
+//     for(int i = 0; i < argc; i++)
+//     {
+//         std::cout << "out" << argv[i] << std::endl;
+//     }
+// #endif
+
+// #if defined(TEST1) 
+//     for(int i = 0; i < argc; i++)
+//     {
+//         std::cerr << "error" << argv[i] << std::endl;
+//         std::clog << "log" << argv[i] << std::endl;
+//     }
+// #endif
 
     return 0;
 }
